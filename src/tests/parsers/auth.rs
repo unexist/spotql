7dcff05d1c812ee1ea6f00dc4102d514ead8d7f5
Christoph Kappel <christoph@unexist.dev>
2021-01-22 17:43:39 +0100

    Fixed naming of vars

--- src/tests/parsers/auth.rs
+++ src/tests/parsers/auth.rs
@@ -14,7 +14,7 @@ use crate::parsers::parser_error::ParserError;
 
 fn parse_auth(input: &[u8]) -> Result<Auth, ParserError> {
     match auth_parser(input) {
-        Ok((_, regular)) => Ok(regular),
+        Ok((_, query)) => Ok(query),
         Err(e) => Err(ParserError {
             message: e.to_string()
         })
@@ -29,9 +29,9 @@ static MESSAGE: &'static str = "p\u{0}\u{0}\u{0}\u{e}test\u{0}";
 #[test]
 fn test_parse_auth() {
     match parse_auth(MESSAGE.as_bytes()) {
-        Ok(regular) => {
-            assert_eq!(regular.tag, 'p');
-            assert_eq!(regular.payload, Some("test"));
+        Ok(auth) => {
+            assert_eq!(auth.tag, 'p');
+            assert_eq!(auth.payload, Some("test"));
         },
         Err(e) => panic!(format!("Error: {}", e)),
     }
